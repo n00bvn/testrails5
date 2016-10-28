9363174a354d297f4f4fb583c8274ba7c57436bc
thogmo <thoth@gmo-media.jp>
2016-10-28 13:34:26 +0700

    updated android-app

--- android-app/app/src/main/java/com/example/thogmo/testr5/MainActivity.java
+++ android-app/app/src/main/java/com/example/thogmo/testr5/MainActivity.java
@@ -1,8 +1,34 @@
 package com.example.thogmo.testr5;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+//import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.res.Configuration;
+//import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+//import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+//import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+//import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.basecamp.turbolinks.TurbolinksSession;
 import com.basecamp.turbolinks.TurbolinksAdapter;
@@ -10,56 +36,17 @@ import com.basecamp.turbolinks.TurbolinksView;
 
 public class MainActivity extends AppCompatActivity implements TurbolinksAdapter {
     // Change the BASE_URL to an address that your VM or device can hit.
-    private static final String BASE_URL = "http://192.168.0.81:3000";
+    private static final String BASE_URL = "http://192.168.0.154:3000";
     private static final String INTENT_URL = "intentUrl";
 
     private String location;
     private TurbolinksView turbolinksView;
 
-    // -----------------------------------------------------------------------
-    // Activity overrides
-    // -----------------------------------------------------------------------
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        // Find the custom TurbolinksView object in your layout
-        turbolinksView = (TurbolinksView) findViewById(R.id.turbolinks_view);
-
-        // For this demo app, we force debug logging on. You will only want to do
-        // this for debug builds of your app (it is off by default)
-        TurbolinksSession.getDefault(this).setDebugLoggingEnabled(true);
-
-        // For this example we set a default location, unless one is passed in through an intent
-        location = getIntent().getStringExtra(INTENT_URL) != null ? getIntent().getStringExtra(INTENT_URL) : BASE_URL;
-
-        // Execute the visit
-        TurbolinksSession.getDefault(this)
-                .activity(this)
-                .adapter(this)
-                .view(turbolinksView)
-                .visit(location);
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-
-        // Since the webView is shared between activities, we need to tell Turbolinks
-        // to load the location from the previous activity upon restarting
-        TurbolinksSession.getDefault(this)
-                .activity(this)
-                .adapter(this)
-                .restoreWithCachedSnapshot(true)
-                .view(turbolinksView)
-                .visit(location);
-    }
-
-    // -----------------------------------------------------------------------
-    // TurbolinksAdapter interface
-    // -----------------------------------------------------------------------
+    private WebView webView;
+    private String mCM;
+    private ValueCallback<Uri> mUploadMessage;
+    private ValueCallback<Uri[]> mUploadMessageArray;
+    private final static int FCR = 1;
 
     @Override
     public void onPageFinished() {
@@ -72,17 +59,17 @@ public class MainActivity extends AppCompatActivity implements TurbolinksAdapter
     }
 
     @Override
-    public void pageInvalidated() {
-
-    }
-
-    @Override
     public void requestFailedWithStatusCode(int statusCode) {
         handleError(statusCode);
     }
 
     @Override
     public void visitCompleted() {
+
+    }
+
+    @Override
+    public void pageInvalidated() {
 
     }
 
@@ -97,10 +84,6 @@ public class MainActivity extends AppCompatActivity implements TurbolinksAdapter
         this.startActivity(intent);
     }
 
-    // -----------------------------------------------------------------------
-    // Private
-    // -----------------------------------------------------------------------
-
     // Simply forwards to an error page, but you could alternatively show your own native screen
     // or do whatever other kind of error handling you want.
     private void handleError(int code) {
@@ -112,5 +95,190 @@ public class MainActivity extends AppCompatActivity implements TurbolinksAdapter
                     .view(turbolinksView)
                     .visit(BASE_URL + "/error");
         }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        // Find the custom TurbolinksView object in your layout
+        turbolinksView = (TurbolinksView) findViewById(R.id.turbolinks_view);
+
+        // For this demo app, we force debug logging on. You will only want to do
+        // this for debug builds of your app (it is off by default)
+        TurbolinksSession.getDefault(this).setDebugLoggingEnabled(true);
+
+        // For this example we set a default location, unless one is passed in through an intent
+        location = getIntent().getStringExtra(INTENT_URL) != null ? getIntent().getStringExtra(INTENT_URL) : BASE_URL;
+
+        webView = TurbolinksSession.getDefault(this).getWebView();
+        WebSettings websettings = webView.getSettings();
+        websettings.setAllowFileAccess(true);
+        websettings.setJavaScriptEnabled(true);
+        websettings.setDomStorageEnabled(true);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }
+
+//        webView.setWebViewClient(new Callback());
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            //For Android 4.1+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                MainActivity.this.startActivityForResult(
+                        Intent.createChooser(i, "Chosse your file"),
+                        MainActivity.FCR
+                );
+            }
+
+            //For Android 5.0+
+            public boolean onShowFileChooser(
+                    WebView webView, ValueCallback<Uri[]> filePathCallback,
+                    FileChooserParams fileChooserParams) {
+                if (mUploadMessageArray != null) {
+                    mUploadMessageArray.onReceiveValue(null);
+                }
+                mUploadMessageArray = filePathCallback;
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(MainActivity.this.getPackageManager()) != null) {
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                        takePictureIntent.putExtra("PhotoPath", mCM);
+                    } catch (IOException ex) {
+                        //Log.e(TAG, "Image file creation failed", ex);
+                    }
+                    if (photoFile != null) {
+                        mCM = "file:" + photoFile.getAbsolutePath();
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                    } else {
+                        takePictureIntent = null;
+                    }
+                }
+                Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                contentSelectionIntent.setType("image/*");
+                Intent[] intentArray;
+                if (takePictureIntent != null) {
+                    intentArray = new Intent[]{takePictureIntent};
+                } else {
+                    intentArray = new Intent[0];
+                }
+
+                Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Chosse your file");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+                startActivityForResult(chooserIntent, FCR);
+                return true;
+            }
+        });
+
+
+        // Execute the visit
+        TurbolinksSession.getDefault(this)
+                .activity(this)
+                .adapter(this)
+                .view(turbolinksView)
+                .visit(location);
+    }
+
+    public class Callback extends WebViewClient {
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
+            Toast.makeText(getApplicationContext(), "Failed loading app!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        // NOTE : We disabled this because when we select the file this runs and it restart the form.
+        //TurbolinksSession.getDefault(this)
+        //        .activity(this)
+        //        .adapter(this)
+        //        .restoreWithCachedSnapshot(true)
+        //        .view(turbolinksView)
+        //        .visit(location);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (Build.VERSION.SDK_INT >= 21) {
+            Uri[] results = null;
+            //Check if response is positive
+            if (resultCode == Activity.RESULT_OK) {
+                if (requestCode == FCR) {
+                    if (null == mUploadMessageArray) {
+                        return;
+                    }
+
+                    if (intent == null) {
+                        //Capture Photo if no image available
+                        if (mCM != null) {
+                            results = new Uri[]{Uri.parse(mCM)};
+                        }
+                    } else {
+                        String dataString = intent.getDataString();
+                        if (dataString != null) {
+                            results = new Uri[]{Uri.parse(dataString)};
+                        }
+                    }
+                }
+            }
+            mUploadMessageArray.onReceiveValue(results);
+            mUploadMessageArray = null;
+
+        } else {
+            if (requestCode == FCR) {
+                if (null == mUploadMessage) return;
+                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+                mUploadMessage.onReceiveValue(result);
+                mUploadMessage = null;
+            }
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, @NonNull KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    // -----------------------------------------------------------------------
+    // Private
+    // -----------------------------------------------------------------------
+
+    // Create an image file
+    private File createImageFile() throws IOException {
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "img_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName, ".jpg", storageDir);
     }
 }
